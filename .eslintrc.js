db8cffb8fba5528d266a705bd27320d3c307c44d
Yathi <bm.thunderemperor@gmail.com>
2019-02-15 09:59:05 -0500

    Added ESLint, Proptypes, Created Movie component, Made a fetch call to moviedb to get data

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+        browser: true, 
+        "es6": true
+    },
+    "settings": {
+        "ecmascript": 6,
+        "jsx": true
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "ecmaFeatures": {
+            "experimentalDecorators": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": ["react"]
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Movie from './Movie';
 class App extends Component {
+  state = {
+    movies: []
+  }
+  async componentDidMount() {
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=6e2437156edf9721bc0a67e47653581f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results
+      })
+
+    } catch(e) {
+      console.log(e);
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Edmonton Movie Club
         </header>
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
       </div>
     );
   }
