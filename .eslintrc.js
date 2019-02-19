fb004ebdd67579740c9cec3bdac7d99b0c1fdd96
Yathi <bm.thunderemperor@gmail.com>
2019-02-19 00:11:58 -0500

    Configure ESlint. Functional stateless component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
-    "extends": "airbnb",
-    "env": {
-        browser: true, 
-        "es6": true
-    },
-    "settings": {
-        "ecmascript": 6,
+  "extends": "airbnb",
+  "env": {
+    browser: true,
+    "es6": true
+  },
+  "settings": {
+    "ecmascript": 6,
+    "jsx": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "ecmaFeatures": {
+        "experimentalDecorators": true,
         "jsx": true
     },
-    "parserOptions": {
-        "ecmaVersion": 2017,
-        "ecmaFeatures": {
-            "experimentalDecorators": true,
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": ["react"]
+    "sourceType": "module"
+  },
+  "plugins": ["react"],
+  "rules": {
+    "react/jsx-filename-extension": 0
+  }
 };
