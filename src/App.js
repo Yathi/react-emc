4e9822f227beb719281ed8fac0aa7411d012179b
Yathi <bm.thunderemperor@gmail.com>
2019-02-19 23:25:12 -0500

    Added router and some routing. Switch and route.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
 import './App.css';
-import Movie from './Movie';
-class App extends Component {
-  state = {
-    movies: []
-  }
-  async componentDidMount() {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=6e2437156edf9721bc0a67e47653581f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const movies = await res.json();
-      console.log(movies);
-      this.setState({
-        movies: movies.results
-      })
+import MoviesList from './MoviesList';
 
-    } catch(e) {
-      console.log(e);
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          Edmonton Movie Club
-        </header>
-        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie}/>)}
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        Edmonton Movie Club
+      </header>
+      <Switch>
+        <Route path="/" component={MoviesList} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
